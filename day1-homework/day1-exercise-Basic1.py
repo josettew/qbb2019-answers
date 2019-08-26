7378e2690d52c195ca6461e1f6861edabeb87b35
Josette Wlaschin <jjosettew@gmail.com>
2019-08-26 17:21:46 -0400

    Basic exercise #1

---
+++ day1-homework/day1-exercise-Basic1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# count number of alignments
+import sys
+# arugment is SRR072893.SAM
+if len(sys.argv)>1: 
+    f = open(sys.argv[1]) #if you have an arugment with atleast 1 character, open the file
+else:
+    f = sys.stdin #otherwise set f = <, take previous output and pipe into the command
+count = 0 #starting point
+for i, line in enumerate(f): 
+    count +=1 #add 1 for each line, starting at 0
+print(count)
