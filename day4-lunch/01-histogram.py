194734f4646503a7319bff5012d1050a8e8f5929
Josette Wlaschin <jjosettew@gmail.com>
2019-08-29 12:44:46 -0400

    lunch exercise 1

---
+++ day4-lunch/01-histogram.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+"""
+Usage: ./01-hist.py <ctab>
+Plot FPKMs
+"""
+## run this:  ./01-histogram.py ../results/stringtie/SRR072893/t_data.ctab 
+
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.stats as stats
+import pandas as pd
+
+
+# fpkms = []
+# for i , line in enumerate(open(sys.argv[1])):
+#     if i == 0: #get rid of header
+#         continue
+#     fields = line.rstrip("\n").split("\t") #split each line and strip white space
+#     #filter out values of 0
+#     if float(fields[11]) > 0:
+#         fpkms.append(float(fields[11]))
+#
+# print(len(fpkms))
+
+ctab = pd.read_csv(sys.argv[1], sep = "\t")
+goi = ctab.loc[:, "FPKM"] > 0 
+#graph a log transformed version of data
+my_data = np.log2(ctab.loc[goi, "FPKM"])
+
+fig, ax = plt.subplots()
+
+a = float(sys.argv[2])
+loc = float(sys.argv[3])
+scale = float(sys.argv[4])
+x = np.linspace(-15, 15, 100)
+y = stats.skewnorm.pdf(x, a, loc, scale)
+
+y_norm = stats.norm.pdf(x, loc, scale)
+ax.plot(x, y_norm)
+
+ax.hist(my_data, bins = 100, density = True)
+#add bell curve line to plot
+ax.plot(x,y,color="green")
+ax.set_title("Histogram For Lunch Exercise 1")
+ax.set_xlabel("log of fpkms")
+ax.set_ylabel("% of frequency")
+
+fig.savefig("fpkms.png")
+plt.close(fig)
