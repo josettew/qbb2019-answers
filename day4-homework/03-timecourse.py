3c63f952ad4c48245f8aed964f83c354816ed42c
Josette Wlaschin <jjosettew@gmail.com>
2019-08-29 21:32:49 -0400

    hmwk exercise 3 with replicates

--- day4-homework/03-timecourse.py
+++ day4-homework/03-timecourse.py
@@ -1,56 +1,63 @@
 #!/usr/bin/env python3
 
 """
-Usage: ./02-timecourse.py <t_name> <samples.csv> <FPKMs>
-create a timecourse of a given transcript for females
+Usage: ./02-timecourse.py <t_name> <samples.csv> <FPKMs.csv>
+
+Create a timecourse of a given transcript for females
+
+./03-timecourse.py FBtr0331261 ~/qbb2019/data/samples.csv replicates.csv ../results/stringtie/
 """
-## input: ./03-timecourse.py FBtr0331261 ~/qbb2019/data/samples.csv all.csv 
 
 import sys
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
+import os
+
+#specify transcript of interest
+t_name= sys.argv[1]
+
+#load metadata
+samples= pd.read_csv(sys.argv[2])
+
+replicates=pd.read_csv(sys.argv[3])
+
+ctab_dir = sys.argv[4]
+
+def sexfinder(sex,rep):
+   soi= rep.loc[:,"sex"] == sex # Identify which sample rows match target sex
+
+   srr_ids = rep.loc[soi, "sample"] # Find the samples, corresponding to rows in `soi`
 
 
-t_name = sys.argv[1]
-samples = pd.read_csv(sys.argv[2])
 
-def sex_sorter(sex):
-   soi = samples.loc[:,"sex"] == sex
-   stages = samples.loc[soi,"stage"]
-   # print(srr_ids)
-
-   #load fpkms
-   fpkms = pd.read_csv(sys.argv[3],index_col="t_name")
-
-   #extract data
-   my_data =[]
-   for stage in stages:
-       # print(srr_id)
-       my_data.append(fpkms.loc[t_name,sex + '_' + stage])
+   my_data = []
+   for srr_id in srr_ids:
+       ctab_path = os.path.join (ctab_dir, srr_id, "t_data.ctab") # Find path to ctab file
+       df = pd.read_csv (ctab_path, sep="\t",index_col="t_name") # Load ctab file
+       my_data.append(df.loc[t_name,"FPKM"]) # Grab the FPKM value for our transcript, and append to my_data
+       # fpkms = pd.read_csv(sys.argv[3], index_col="t_name")
+       # my_data.append(fpkms.loc[t_name,srr_id])
+   #print(sex, rep, my_data)
    return my_data
 
-male_data = sex_sorter("male")
-female_data = sex_sorter("female")
-#male_data_last4 = male_data[4:8]
-#female_data_last4 = female_data[4:8]
-labelz = ["male", "female"]
-label2 = ["10", "11", "12", "13", "14A", "14B", "14C", "14D"]
+female_fpkms = sexfinder('female', samples)
+female_fpkms2 = sexfinder('female', replicates)
+male_fpkms = sexfinder('male', samples)
+male_fpkms2 = sexfinder('male', replicates)
 
-# print(my_data)
+fig, (ax) =plt.subplots()
+ax.plot(female_fpkms, color="red", label='female')
+ax.plot(range(4,8),female_fpkms2,"x", color="red", label='female')
 
-fig,ax = plt.subplots()
-ax.plot(male_data, color="blue")
-#ax.plot(male_data_last4, color="green")
-ax.plot(female_data, color="red")
-#ax.plot(female_data_last4, color="orange")
-ax.set_title("Homework Exercise 3 Sxl Males and Females")
-ax.set_xlabel("devlopmental stage")
-ax.set_xticklabels(label2, rotation = "vertical")
-ax.set_xticks(np.arange(len(label2)))
+ax.plot(male_fpkms, color="blue", label='male')
+ax.plot(range(4,8),male_fpkms2,"x", color="blue", label='male')
 
-ax.set_ylabel("mRNA abundance (RPKM)")
-plt.legend( labels = labelz, loc='center left', bbox_to_anchor=(1, .5))
+ax.legend(loc='lower right', bbox_to_anchor=(1.3, .5))
+ax.set_xticklabels(["0", "10", "11", "12", "13", "14A", "14B", "14C", "14D"])
+ax.set_ylabel("mRNA abundance(RPKM)")
+ax.set_xlabel("development stage")
+fig.suptitle("Sxl", style="italic")
 plt.tight_layout()
-fig.savefig("timecourse.png")
+plt.subplots_adjust(top=0.9)
+fig.savefig("timecourse2.png")
 plt.close(fig)
