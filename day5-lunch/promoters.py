f22f18e367aa5078bafc7f4db32c7cce6869c03e
Josette Wlaschin <jjosettew@gmail.com>
2019-08-30 13:18:21 -0400

    lunch exercise 2

--- day5-lunch/promoters.py
+++ day5-lunch/promoters.py
@@ -13,15 +13,26 @@ ctab = pd.read_csv(sys.argv[1], sep = "\t")
 all_start_end_values = ctab.loc[:,["chr", "strand", "t_name", "start", "end"]]
 
 
-print("Chr" + "\t" + "t_name" + "\t" + "promoter start" + "\t" + "promoter end") #labels
+#print("Chr" + "\t" + "promoter start" + "\t" + "promoter end" + "\t" + "t_name") #labels
 for index, row in all_start_end_values.iterrows(): #iterate thru rows of data frame
    if row.loc["strand"] == "+":
        promoterStart = int(row.loc["start"])
        promoter_start_plus_500 = promoterStart + 500
        promoter_start_minus_500 = promoterStart - 500
-       print(str(row.loc["chr"]) + "\t" + str(row.loc["t_name"]) + "\t" + str(promoter_start_minus_500) + "\t" + str( promoter_start_plus_500))
+       if promoter_start_plus_500 <= 0:
+           promoter_start_plue_500 = 1
+       if promoter_start_minus_500 <= 0:
+           promoter_start_minus_500 = 1
+       print(str(row.loc["chr"])  + "\t" + str(promoter_start_minus_500) + "\t" + str( promoter_start_plus_500) + "\t" + str(row.loc["t_name"]))
    elif row.loc["strand"] == "-":
        promoterStart = int(row.loc["end"])
        promoter_start_plus_500 = promoterStart + 500
        promoter_start_minus_500 = promoterStart - 500
-       print(str(row.loc["chr"]) + "\t" + str(row.loc["t_name"]) + "\t" + str(promoter_start_minus_500) + "\t" + str( promoter_start_plus_500))
+       if promoter_start_plus_500 <= 0:
+           promoter_start_plue_500 = 1
+       if promoter_start_minus_500 <= 0:
+           promoter_start_minus_500 = 1
+       print(str(row.loc["chr"]) + "\t" + str(promoter_start_minus_500) + "\t" + str( promoter_start_plus_500) + "\t" + str(row.loc["t_name"]))
+       
+       
+       
