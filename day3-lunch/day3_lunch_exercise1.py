1a217fc51852bb3d49cd38bef7f569a93070bc5a
Josette Wlaschin <jjosettew@gmail.com>
2019-09-02 14:52:56 -0400

    completed day3 lunch task

--- day3-lunch/day3_lunch_exercise1.py
+++ day3-lunch/day3_lunch_exercise1.py
@@ -15,7 +15,7 @@ for line in open(sys.argv[1]):
             if i == "gene_name":
                 gene_name = fields[count]
                 break
-        gene_list.append((gene_name, fields[3], fields[4]))
+        gene_list.append((gene_name, int(fields[3]), int(fields[4])))
 ## creates my list with the gene name, the start position and end position
 
 ##Binary Search
@@ -29,23 +29,24 @@ while (lo <= hi):
    mid = int((hi + lo) / 2)
    number_iterations = number_iterations +1
    if mid == hi or mid == lo:
-       print(gene_list[mid][0])
-       print(number_iterations)
+       #print(gene_list[mid][0])
+       #print(number_iterations)
        break
    if ( mutation < int(gene_list[mid][1])):
        hi = mid
    elif (mutation > int(gene_list[mid][1])):
        lo = mid
-   else:
-       print(gene_list[mid][0])
-       print(number_iterations)
-       break
+       
+if (abs(gene_list[lo][2] - mutation)) > (abs(gene_list[hi][1]-mutation)):
+    mid = hi
+    print(abs(gene_list[hi][1]-mutation))
+if (abs(gene_list[lo][2] - mutation)) < (abs(gene_list[hi][1]-mutation)):
+    mid = lo
+    print (abs(gene_list[lo][2] - mutation))
 
-#did not have enough time to calculate distance
- 
+print(gene_list[mid])
+print (number_iterations)
 
-
-
-
-    
-    
+#27 bp away
+#tin is gene_name
+#takes 12 interations
